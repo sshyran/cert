2324bbd2b942dbbfb3208d469384d81304ed1033
Ivan Andreev <ivandeex@gmail.com>
2018-08-13 01:37:14 +0300

    cert-replica: run the first push

--- roles/cert-replica/templates/certbot-post-receive.sh
+++ roles/cert-replica/templates/certbot-post-receive.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 LOG=/var/log/letsencrypt/letsencrypt.log
 echo "$(date): start post-push actions" >> $LOG
-# fix directory permission
-chown root:{{ certbot_group }} /etc/letsencrypt/{archive,live}
-chmod 0750 /etc/letsencrypt/{archive,live}
+
+# move temporary files to /etc/letsencrypt
+(
+    cd ~{{ certbot_replica_user }}
+    chown -R root:root .letsencrypt.tmp
+    for subdir in archive live ; do
+        rsync -a --delete \
+              .letsencrypt.tmp/$subdir/ \
+              /etc/letsencrypt/$subdir/
+        # fix directory permissions
+        chown root:{{ certbot_group }} /etc/letsencrypt/$subdir
+        chmod 0750 /etc/letsencrypt/$subdir
+    done
+    rm -rf .letsencrypt.tmp
+) >> $LOG 2>&1
+
+# run hook scripts
 for script in /etc/letsencrypt/renewal-hooks/{pre,deploy,post}/*
 do
     if [ -x $script ]; then
-        echo "$(date): running $script" >> $LOG
-        $script >> $LOG 2>&1
+        echo "$(date): running $script"
+        $script
     fi
-done
+done  >> $LOG 2>&1
+
+# final message
 echo "$(date): pushed successfully" >> $LOG
 echo "pushed successfully"
